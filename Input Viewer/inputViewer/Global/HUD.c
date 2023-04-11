99f2f201c2195ac42cdd0247d08fd88e6dda4c17
Kiaru-the-Fox <supersonicchaos00@gmail.com>
2023-04-11 17:42:31 -0400

    dox your inputs, NOW

---
+++ Input Viewer/inputViewer/Global/HUD.c
@@ -0,0 +1,159 @@
+#include "../../GameAPI/C/GameAPI/Game.h"
+#include "HUD.h"
+
+ObjectHUD* HUD;
+ModObjectHUD* ModHUD;
+
+void HUD_Update()
+{
+    RSDK_THIS(HUD);
+    Mod.Super(HUD->classID, SUPER_UPDATE, NULL);
+
+    HUD_HandleInputViewer();
+}
+
+void HUD_Draw()
+{
+    Mod.Super(HUD->classID, SUPER_DRAW, NULL);
+    RSDK_THIS(HUD);
+
+    Vector2 drawPos, inputPos;
+    inputPos.x = ModHUD->inputPos.x;
+    inputPos.y = ModHUD->inputPos.y;
+
+    // D-pad
+    drawPos.x = inputPos.x;
+    drawPos.y = inputPos.y - TO_FIXED(6);
+    RSDK.DrawSprite(&ModHUD->inputDpadAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputUpAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputDownAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputLeftAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputRightAnimator, &drawPos, true);
+
+    // ABC
+    drawPos.x = inputPos.x + TO_FIXED(28);
+    drawPos.y = inputPos.y - TO_FIXED(6);
+    RSDK.DrawSprite(&ModHUD->inputABCAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputAAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputBAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputCAnimator, &drawPos, true);
+
+    // XYZ
+    drawPos.x = inputPos.x + TO_FIXED(28);
+    drawPos.y = inputPos.y - TO_FIXED(18);
+    RSDK.DrawSprite(&ModHUD->inputXYZAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputXAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputYAnimator, &drawPos, true);
+    RSDK.DrawSprite(&ModHUD->inputZAnimator, &drawPos, true);
+}
+
+void HUD_Create(void* data)
+{
+    Mod.Super(HUD->classID, SUPER_CREATE, data);
+    RSDK_THIS(HUD);
+
+    ModHUD->inputPos.x = TO_FIXED(16);
+    ModHUD->inputPos.y = TO_FIXED(ScreenInfo->size.y - 32);
+
+    // D-Pad
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 0, &ModHUD->inputDpadAnimator, true, 0);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 1, &ModHUD->inputUpAnimator, true, 1);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 1, &ModHUD->inputDownAnimator, true, 2);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 1, &ModHUD->inputLeftAnimator, true, 3);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 1, &ModHUD->inputRightAnimator, true, 4);
+
+    // ABC
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 2, &ModHUD->inputABCAnimator, true, 0);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 3, &ModHUD->inputAAnimator, true, 1);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 3, &ModHUD->inputBAnimator, true, 2);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 3, &ModHUD->inputCAnimator, true, 3);
+
+    // XYZ
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 4, &ModHUD->inputXYZAnimator, true, 0);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 5, &ModHUD->inputXAnimator, true, 1);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 5, &ModHUD->inputYAnimator, true, 2);
+    RSDK.SetSpriteAnimation(ModHUD->inputViewFrames, 5, &ModHUD->inputZAnimator, true, 3);
+}
+
+void HUD_StageLoad()
+{
+    Mod.Super(HUD->classID, SUPER_STAGELOAD, NULL);
+    ModHUD->inputViewFrames = RSDK.LoadSpriteAnimation("Global/InputView.bin", SCOPE_STAGE);
+}
+
+void HUD_HandleInputViewer(void)
+{
+    RSDK_THIS(HUD);
+    EntityPlayer* leader = RSDK_GET_ENTITY(SLOT_PLAYER1, Player);
+    RSDKControllerState* controller = &ControllerInfo[leader->controllerID];
+
+    if (controller->keyUp.press || controller->keyUp.down) {
+        ModHUD->inputUpAnimator.frameID = 1;
+    }
+    else {
+        ModHUD->inputUpAnimator.frameID = 0;
+    }
+
+    if (controller->keyDown.press || controller->keyDown.down) {
+        ModHUD->inputDownAnimator.frameID = 2;
+    }
+    else {
+        ModHUD->inputDownAnimator.frameID = 0;
+    }
+
+    if (controller->keyLeft.press || controller->keyLeft.down) {
+        ModHUD->inputLeftAnimator.frameID = 3;
+    }
+    else {
+        ModHUD->inputLeftAnimator.frameID = 0;
+    }
+
+    if (controller->keyRight.press || controller->keyRight.down) {
+        ModHUD->inputRightAnimator.frameID = 4;
+    }
+    else {
+        ModHUD->inputRightAnimator.frameID = 0;
+    }
+
+    if (controller->keyA.press || controller->keyA.down) {
+        ModHUD->inputAAnimator.frameID = 1;
+    }
+    else {
+        ModHUD->inputAAnimator.frameID = 0;
+    }
+
+    if (controller->keyB.press || controller->keyB.down) {
+        ModHUD->inputBAnimator.frameID = 2;
+    }
+    else {
+        ModHUD->inputBAnimator.frameID = 0;
+    }
+
+    if (controller->keyC.press || controller->keyC.down) {
+        ModHUD->inputCAnimator.frameID = 3;
+    }
+    else {
+        ModHUD->inputCAnimator.frameID = 0;
+    }
+
+    if (controller->keyX.press || controller->keyX.down) {
+        ModHUD->inputXAnimator.frameID = 1;
+    }
+    else {
+        ModHUD->inputXAnimator.frameID = 0;
+    }
+
+    if (controller->keyY.press || controller->keyY.down) {
+        ModHUD->inputYAnimator.frameID = 2;
+    }
+    else {
+        ModHUD->inputYAnimator.frameID = 0;
+    }
+
+    if (controller->keyZ.press || controller->keyZ.down) {
+        ModHUD->inputZAnimator.frameID = 3;
+    }
+    else {
+        ModHUD->inputZAnimator.frameID = 0;
+    }
+}
---
+++ Input Viewer/inputViewer/Global/HUD.h
@@ -0,0 +1,220 @@
+#ifndef OBJ_HUD_H
+#define OBJ_HUD_H
+
+#include "../../GameAPI/C/GameAPI/Game.h"
+
+typedef enum {
+    HUDOFF_SCORE,
+    HUDOFF_TIME,
+    HUDOFF_RINGS,
+    HUDOFF_LIFE,
+} HUDOffsetTypes;
+
+typedef enum {
+    KEY_A,
+    KEY_B,
+    KEY_X,
+    KEY_Y,
+    KEY_START,
+} KeyIDs;
+
+// Object Class
+typedef struct {
+    RSDK_OBJECT
+        uint16 aniFrames;
+#if GAME_VERSION != VER_100
+    uint16 superButtonFrames;
+#endif
+#if MANIA_USE_PLUS
+    uint16 sfxClick;
+    uint16 sfxStarpost;
+    bool32 showTAPrompt;
+    bool32 replaySaveEnabled;
+    int32 screenBorderType[PLAYER_COUNT];
+    int32 swapCooldown;
+    int32 stockFlashTimers[PLAYER_COUNT];
+#endif
+} ObjectHUD;
+
+// Entity Class
+typedef struct {
+    RSDK_ENTITY
+        StateMachine(state);
+    Vector2 scorePos;
+    Vector2 timePos;
+    Vector2 ringsPos;
+    Vector2 lifePos;
+#if MANIA_USE_PLUS
+    int32 lifeFrameIDs[PLAYER_COUNT];
+    int32 lives[PLAYER_COUNT];
+#endif
+    int32 targetPos;
+#if GAME_VERSION != VER_100
+    int32 actionPromptPos;
+#endif
+#if MANIA_USE_PLUS
+    StateMachine(vsStates[PLAYER_COUNT]);
+    Vector2 vsScorePos[PLAYER_COUNT];
+    Vector2 vsTimePos[PLAYER_COUNT];
+    Vector2 vsRingsPos[PLAYER_COUNT];
+    Vector2 vsLifePos[PLAYER_COUNT];
+    int32 vsTargetPos[PLAYER_COUNT];
+    int32 screenID;
+#endif
+#if GAME_VERSION != VER_100
+    int32 timeFlashFrame;
+#endif
+    int32 ringFlashFrame;
+    bool32 enableTimeFlash;
+    bool32 enableRingFlash;
+    Animator hudElementsAnimator;
+    Animator numbersAnimator;
+    Animator hyperNumbersAnimator;
+    Animator lifeIconAnimator;
+    Animator playerIDAnimator;
+#if GAME_VERSION != VER_100
+    Animator superIconAnimator;
+    Animator superButtonAnimator;
+#endif
+    Animator thumbsUpIconAnimator;
+    Animator thumbsUpButtonAnimator;
+    Animator replayClapAnimator;
+    Animator saveReplayButtonAnimator;
+} EntityHUD;
+
+typedef struct {
+    uint16 inputViewFrames;
+    Vector2 inputPos;
+    Animator inputDpadAnimator;
+    Animator inputABCAnimator;
+    Animator inputXYZAnimator;
+    Animator inputUpAnimator;
+    Animator inputDownAnimator;
+    Animator inputLeftAnimator;
+    Animator inputRightAnimator;
+    Animator inputAAnimator;
+    Animator inputBAnimator;
+    Animator inputCAnimator;
+    Animator inputXAnimator;
+    Animator inputYAnimator;
+    Animator inputZAnimator;
+} ModObjectHUD;
+
+// Object Struct
+extern ObjectHUD* HUD;
+extern ModObjectHUD* ModHUD;
+
+// Entity Class
+typedef struct {
+    RSDK_ENTITY
+    StateMachine(state);
+    StateMachine(nextAirState);
+    StateMachine(nextGroundState);
+    void* camera;
+    Animator animator;
+    Animator tailAnimator;
+    int32 minJogVelocity;
+    int32 minRunVelocity;
+    int32 minDashVelocity;
+    int32 unused; // the only used variable in the player struct, I cant find a ref to it anywhere so...
+    int32 tailRotation;
+    int32 tailDirection;
+    uint16 aniFrames;
+    uint16 tailFrames;
+    uint16 animationReserve; // what anim to return to after SpringTwirl/SpringDiagonal has finished and the player is falling downwards
+    uint16 playerID;
+    Hitbox* outerbox;
+    Hitbox* innerbox;
+    int32 characterID;
+    int32 rings;
+    int32 ringExtraLife;
+    int32 shield;
+    int32 lives;
+    int32 score;
+    int32 score1UP;
+    bool32 hyperRing;
+    int32 timer;
+    int32 outtaHereTimer;
+    int32 abilityTimer;
+    int32 spindashCharge;
+    int32 abilityValue;
+    int32 drownTimer;
+    int32 invincibleTimer;
+    int32 speedShoesTimer;
+    int32 blinkTimer;
+    int32 scrollDelay;
+    int32 skidding;
+    int32 pushing;
+    int32 underwater;     // 0 = not in water, 1 = in palette water, else water entityID
+    bool32 groundedStore; // prev frame's onGround value
+    bool32 invertGravity;
+    bool32 isChibi;
+#if GAME_VERSION != VER_100
+    bool32 isTransforming;
+#endif
+    int32 superState;
+    int32 superRingLossTimer;
+    int32 superBlendAmount;
+    int32 superBlendState;
+    bool32 sidekick;
+    int32 scoreBonus;
+    int32 jumpOffset;
+    int32 collisionFlagH;
+    int32 collisionFlagV;
+    int32 topSpeed;
+    int32 acceleration;
+    int32 deceleration;
+    int32 airAcceleration;
+    int32 airDeceleration;
+    int32 skidSpeed;
+    int32 rollingFriction;
+    int32 rollingDeceleration;
+    int32 gravityStrength;
+    int32 abilitySpeed;
+    int32 jumpStrength;
+    int32 jumpCap;
+    int32 flailing;
+    int32 sensorX[5];
+    int32 sensorY;
+    Vector2 moveLayerPosition;
+#if MANIA_USE_PLUS
+    StateMachine(stateInputReplay);
+#endif
+    StateMachine(stateInput);
+    int32 controllerID;
+    int32 controlLock;
+    bool32 up;
+    bool32 down;
+    bool32 left;
+    bool32 right;
+    bool32 jumpPress;
+    bool32 jumpHold;
+    bool32 applyJumpCap;
+    int32 jumpAbilityState;
+    StateMachine(stateAbility);
+    StateMachine(statePeelout);
+    int32 flyCarryTimer;
+    Vector2 flyCarrySidekickPos;
+    Vector2 flyCarryLeaderPos;
+    uint8 deathType;
+    bool32 forceRespawn;
+#if MANIA_USE_PLUS
+    bool32 isGhost;
+#endif
+    int32 abilityValues[8];
+    void* abilityPtrs[8];
+#if MANIA_USE_PLUS
+    int32 uncurlTimer;
+#endif
+} EntityPlayer;
+
+extern EntityPlayer* Player;
+
+// Standard Entity Events
+void HUD_Update();
+void HUD_Draw();
+void HUD_Create(void* data);
+void HUD_StageLoad();
+void HUD_HandleInputViewer(void);
+
+#endif //! OBJ_HUD_H
